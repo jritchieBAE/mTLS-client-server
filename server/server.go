38d8d20d9ebeb142cdeadf589df5ef8e56c2bed5
James Ritchie <james.g.ritchie@baesystems.com>
2019-02-12 14:20:49 +0000

    added enum to select which server type to start

--- server/server.go
+++ server/server.go
@@ -12,16 +12,44 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!\n")
 }
 
+type TLSType int
+
+const (
+	None TLSType = 0
+	TLS  TLSType = 1
+	mTLS TLSType = 2
+)
+
 func main() {
 
 	http.HandleFunc("/hello", helloHandler)
 
-	// server, err := (mtls.NewTLSServer().WithCertificates("../cert.pem", "../key.pem"))
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// log.Fatal(server.Listen(":8443"))
+	serverType := TLS
 
 	server := mtls.NewTLSServer()
-	log.Fatal(server.Listen(":8080"))
+
+	switch serverType {
+	case None:
+		{
+			log.Fatal(server.ListenNoTLS(":8443"))
+		}
+
+	case TLS:
+		{
+			server, err := (mtls.NewTLSServer().WithCertificates("../cert.pem", "../key.pem"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Fatal(server.ListenTLS(":8443"))
+		}
+
+	case mTLS:
+		{
+			server, err := (mtls.NewTLSServer().WithCertificates("../cert.pem", "../key.pem"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Fatal(server.ListenMTLS(":8443"))
+		}
+	}
 }
