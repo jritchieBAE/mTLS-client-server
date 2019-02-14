96eb9be9e21ee7586c0af77c61b708c5f8f87248
James Ritchie <james.g.ritchie@baesystems.com>
2019-02-14 14:34:10 +0000

    updated filenames

--- makecerts.sh
+++ makecerts.sh
@@ -1,1 +1,3 @@
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out cert.pem -keyout key.pem -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=Unit/CN=localhost"
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out ca.crt -keyout ca.key -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=Unit/CN=localhost"
+openssl req -new -key ca.key -out ca.csr -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=Unit/CN=localhost"
+openssl x509 -req -in ca.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out localhost.crt
