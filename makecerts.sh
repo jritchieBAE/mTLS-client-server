f384a0a53b91a3d48a139533f1ce23f32ecc630f
James Ritchie <james.g.ritchie@baesystems.com>
2019-02-19 12:21:53 +0000

    generate root certs and server/client signed certs

--- makecerts.sh
+++ makecerts.sh
@@ -1,3 +1,17 @@
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out ca.crt -keyout ca.key -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=Unit/CN=localhost"
-openssl req -new -key ca.key -out ca.csr -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=Unit/CN=localhost"
-openssl x509 -req -in ca.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out localhost.crt
+# Create root certificate
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -out root.crt -keyout root.key -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=Root/CN=localhost"
+
+# install root certificates
+# echo "Installing root certificate"
+# sudo cp root.crt /etc/pki/ca-trust/source/anchors
+# sudo update-ca-trust extract
+
+# create Prometheus certificate & key
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout client.key -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=Prometheus/CN=localhost"
+openssl req -new -key client.key -out client.csr -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=Prometheus/CN=localhost"
+openssl x509 -req -in client.csr -CA root.crt -CAkey root.key -CAcreateserial -out client.crt
+
+# create Node certificate & key
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout server.key -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=NodeExporter/CN=localhost"
+openssl req -new -key server.key -out server.csr -subj "/C=US/ST=California/L=Mountain View/O=Organisation/OU=NodeExporter/CN=localhost"
+openssl x509 -req -in server.csr -CA root.crt -CAkey root.key -CAcreateserial -out server.crt
