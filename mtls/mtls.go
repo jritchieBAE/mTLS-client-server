3dd06c39a823e92a35060a15803e93d08c10ef4d
James Ritchie <james.g.ritchie@baesystems.com>
2019-02-19 12:33:10 +0000

    added CA cert to mtls server

--- mtls/mtls.go
+++ mtls/mtls.go
@@ -20,29 +20,27 @@ func (t *TlsServer) Listen(address string) error {
 	}
 }
 
-func NewMtlsServer(certPath, keyPath, CAcertPath string) (*TlsServer, error) {
+func NewMtlsServer(certPath, keyPath, caCertPath string) (*TlsServer, error) {
 
-	caCert, err := ioutil.ReadFile(CAcertPath)
+	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = ioutil.ReadFile(keyPath)
+	caCert, err := ioutil.ReadFile(caCertPath)
 	if err != nil {
 		return nil, err
 	}
 
-	_, err = ioutil.ReadFile(certPath)
-	if err != nil {
-		return nil, err
+	clientCAs := x509.NewCertPool()
+	if ok := clientCAs.AppendCertsFromPEM(caCert); !ok {
+		return nil, errors.New("Failed to parse CA Certificate")
 	}
-
-	caCertPool, _ := x509.SystemCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
 
 	tlsConfig := &tls.Config{
-		RootCAs:    caCertPool,
-		ClientAuth: tls.RequireAndVerifyClientCert,
+		ClientCAs:    clientCAs,
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		Certificates: []tls.Certificate{cert},
 	}
 	tlsConfig.BuildNameToCertificate()
 
@@ -93,24 +91,31 @@ func (t *TlsClient) Get(url string) (*http.Response, error) {
 	return t.get(url)
 }
 
-func NewMtlsClient(certPath, keyPath string) (*TlsClient, error) {
+func NewMtlsClient(certPath, keyPath, caCertPath string) (*TlsClient, error) {
 
 	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
 	if err != nil {
 		return nil, err
 	}
 
-	caCert, _ := ioutil.ReadFile(certPath)
+	caCert, err := ioutil.ReadFile(caCertPath)
+	if err != nil {
+		return nil, err
+	}
 
-	caCertPool, _ := x509.SystemCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
+	clientCAs, _ := x509.SystemCertPool()
+	if ok := clientCAs.AppendCertsFromPEM(caCert); !ok {
+		return nil, errors.New("Failed to parse CA Certificate")
+	}
+
+	tlsConfig := &tls.Config{
+		RootCAs:      clientCAs,
+		Certificates: []tls.Certificate{cert},
+	}
 
 	client := &http.Client{
 		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs:      caCertPool,
-				Certificates: []tls.Certificate{cert},
-			},
+			TLSClientConfig: tlsConfig,
 		},
 	}
 
